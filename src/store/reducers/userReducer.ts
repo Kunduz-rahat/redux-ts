878c71a21e15f462a63c740ee758726a41e45ead
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-09-04 11:41:00 +0600

    add store and userReducer

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,31 +1,23 @@
-const FETCH_USERS = "FETCH_USERS";
-const FETCH_USERS_ERROR = "FETCH_USERS_ERROR";
-const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
-interface UserState {
-  users: any;
-  loading: boolean;
-  error: null | boolean;
-}
+import { UserAction, UserActionTypes, UserState } from "../../types/user";
+
 
 const initialState: UserState = {
-  users: [],
-  loading: false,
-  error: null,
+	users: [],
+	loading: false,
+	error: null,
 };
 
-interface UserAction{
-	type:string,
-	payload?:any
-}
-export const UserReducer = (state = initialState, action:UserAction): UserState => {
-  switch (action.type) {
-    case FETCH_USERS:
-      return { loading: true, error: null, users: [] };
-		case FETCH_USERS_SUCCESS:
-      return { loading: false, error: null, users: action.payload };
-		case FETCH_USERS_ERROR:
-      return { loading: false, error: action.payload,users: [] };
+
+
+export const UserReducer = (state = initialState, action: UserAction): UserState => {
+	switch (action.type) {
+		case UserActionTypes.FETCH_USERS:
+			return { loading: true, error: null, users: [] };
+		case UserActionTypes.FETCH_USERS_SUCCESS:
+			return { loading: false, error: null, users: action.payload };
+		case UserActionTypes.FETCH_USERS_ERROR:
+			return { loading: false, error: action.payload, users: [] };
 		default:
 			return state
-  }
+	}
 };
